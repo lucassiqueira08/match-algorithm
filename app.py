915f677a4704b0176541929161ea3aa494f05350
Lucas Siqueira <lalvessiqueira8@gmail.com>
2020-05-17 16:38:47 -0300

    convert string to lowercase

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from services.similarity import SimilarityService
 
 def main(reference, target):
     similarity_service = SimilarityService()
-    return similarity_service.calculate(reference, target)
+    return similarity_service.calculate(reference.lower(), target.lower())
 
 
 # if __name__ == "__main__":
-#     result = main('caneta azul', 'azul caneta')
+#     result = main('CAneta azul', 'azul caneta')
 #     print(result)
