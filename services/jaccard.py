1542795e14819369d645f4e1519ff8a4f37f225b
bruno1576 <brunosalve1@hotmail.com>
2020-11-02 00:07:39 -0300

    Ajuste formula Dissemelhança para formula da semelhança

--- services/jaccard.py
+++ services/jaccard.py
@@ -2,6 +2,5 @@ def compute_jaccard_similarity_score(element_x, element_y):
     ex = set(element_x.split())
     ey = set(element_y.split())
     result = len(ex.intersection(ey)) / ( (len(ex) + len(ey))  - len(ex.intersection(ey)) ) 
-    similarity = (1 - result) * 100
+    similarity = result * 100
     return similarity
-
