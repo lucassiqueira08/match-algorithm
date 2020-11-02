2847d062274a155ee6637b72ed813145229d8f1a
bruno1576 <brunosalve1@hotmail.com>
2020-11-01 23:08:06 -0300

    Ajuste da função de jaccard

--- services/jaccard.py
+++ services/jaccard.py
@@ -1,9 +1,7 @@
 def compute_jaccard_similarity_score(element_x, element_y):
     ex = set(element_x.split())
     ey = set(element_y.split())
-    result = (len(ex.union(ey)) - len(ex.intersection(ey))) / len(
-        ex.union(ey)
-    )
+    result = len(ex.intersection(ey)) / ( (len(ex) + len(ey))  - len(ex.intersection(ey)) ) 
     similarity = (1 - result) * 100
     return similarity
 
